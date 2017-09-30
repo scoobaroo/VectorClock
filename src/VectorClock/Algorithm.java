adb1347bf5c9887ec7dd9ef33b61e3e1a2fa0030
Mayuri Wadkar <mayuri-wad-012447851@users.noreply.github.com>
2017-09-30 14:15:41 -0700

    changed Algorithm.java
    I have started with Algorithm class first, as I needed some input from that class to Processor class.
    I have mentioned comments in Algorithm class for parts to be done..

--- src/VectorClock/Algorithm.java
+++ src/VectorClock/Algorithm.java
@@ -1,9 +1,58 @@
 package VectorClock;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+
 public class Algorithm {
 
+	public Map<Processor, List<Event>> getExecutionPlan(String filepath) throws IOException {
+		
+		Map <Processor,List<Event>> executionPlan = new HashMap <Processor,List<Event>> ();
+		
+		
+		
+		Scanner fileScanner = new Scanner(filepath);
+		//open file mentioned in filepath
+		//fetch processor and and its list of events from text file
+		//store that data in map executionPlan with key as a Processor and its value as a list of events
+		String line = fileScanner.nextLine();
+		
+		return executionPlan; 
+	}
+	
+	public void execute(Map <Processor,List<Event>> executionPlan) {
+		
+		Iterator<Entry<Processor, List<Event>>> iter = executionPlan.entrySet().iterator();
+		while(iter.hasNext()) {
+			Processor p = iter.next().getKey();
+			p.start();
+		}
+	}
 	
 	public static void main(String[] args) {
-		System.out.println("inside main");
+		Algorithm algo = new Algorithm();
+		
+		//get execution plan from text file
+		Map<Processor, List<Event>> executionPlan;
+		try {
+			executionPlan = algo.getExecutionPlan(args[1]);
+			algo.execute(executionPlan);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//execute the execution plan and compute vector clocks for all processors
+		
 	}
 }
--- src/VectorClock/Processor.java
+++ src/VectorClock/Processor.java
@@ -1,12 +1,18 @@
 package VectorClock;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
 public class Processor extends Thread implements Observer {
 	
-	
     public void update(Observable observable, Object arg) {
 		Buffer b = (Buffer) observable;
 		Message msg = b.getMessage();
     }
+    
+    public void run(List<Event> listOfEvents) {
+		// TODO Auto-generated method stub
+		
+	}
+    
 }
